f051bdbddcbd6a7347c1672d0fac81a8bf89abf8
HuynhNguyenV <nguyenviethuynh1996.vih@gmail.com>
2018-10-30 14:28:00 +0700

    Create rest api

---
+++ src/main/java/com/softech/easynotes/repository/NoteRepository.java
@@ -0,0 +1,11 @@
+package com.softech.easynotes.repository;
+
+import com.softech.easynotes.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+
+}
